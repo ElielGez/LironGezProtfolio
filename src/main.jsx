858fe90e87f5262f656b20638b827ed27bdb6cff
Eliel Gez <eliel.gez@au10tix.com>
2024-04-18 14:43:07 +0300

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import '../node_modules/animate.css/animate.min.css';
 import LayoutContextProvider from './contexts/LayoutContext.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import AssetsManifestProvider from './contexts/AssetsManifestContext.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
+		<Analytics />
 		<BrowserRouter>
 			<AssetsManifestProvider>
 				<LayoutContextProvider>
